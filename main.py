df2dcb28441f873ce60282b2e1940d66344926e0
BerdyshevEugene <berdyshevevg@mail.ru>
2023-05-13 11:29:39 +0300

    feat: registration and authorization add

--- main.py
+++ main.py
@@ -1,73 +1,72 @@
 from datetime import datetime
 from enum import Enum
 from typing import List, Optional
+
+from fastapi_users import fastapi_users, FastAPIUsers
 from pydantic import BaseModel, Field
 
 from fastapi import FastAPI, Request, status
 from fastapi.encoders import jsonable_encoder
 from fastapi.exceptions import ValidationError
-from fastapi.response import JSONResponse
+
+from auth.auth import auth_backend
+from auth.database import User
+from auth.manager import get_user_manager
+from auth.schemas import UserRead, UserCreate
+
+
+fastapi_users = FastAPIUsers[User, int](
+    get_user_manager,
+    [auth_backend],
+)
 
 # создаем приложение
 app = FastAPI(
-    title='Trading app'
+    title='Trade app'
 )
 
-fake_users = [
-    {'id': 1, 'role': 'admin', 'name': 'bob'},
-    {'id': 2, 'role': 'investor', 'name': 'john'},
-    {'id': 3, 'role': 'trader', 'name': 'Matt'},
-    {'id': 4, 'role': 'investor', 'name': 'Homer', 'degree': [
-        {'id': 1, 'created_at': '2019-06-24T09:00', 'type_degree': 'expert'}
-    ]},
-]
+# роутер
+app.include_router(
+    fastapi_users.get_auth_router(auth_backend),
+    prefix='/auth/jwt',
+    tags=['auth'],
+)
 
-
-class DegreeType(Enum):
-    newbie = 'newbie'
-    expert = 'expert'
-
-
-class Degree(BaseModel):
-    id: int
-    created_at: datetime
-    type_degree: DegreeType
-
-
-class User(BaseModel):
-    id: int
-    role: str
-    name: str
-    degree: Optional[List[Degree]]
+# роутер для регистрации пользователя
+app.include_router(
+    fastapi_users.get_register_router(UserRead, UserCreate),
+    prefix="/auth",
+    tags=["auth"],
+)
 
 
 # создаем точку входа для пользователей, для получения данных
-@app.get('/users/{user_id}', response_model=List[User])
-def get_user(user_id: int):
-    return [user for user in fake_users if user.get('id') == user_id]
+# @app.get('/users/{user_id}', response_model=List[User])
+# def get_user(user_id: int):
+#     return [user for user in fake_users if user.get('id') == user_id]
 
 
-fake_trades = [
-    {'id': 1, 'user_id': 1, 'currency': 'BTC',
-        'side': 'buy', 'price': 123, 'amount': 2.12},
-    {'id': 2, 'user_id': 1, 'currency': 'BTC',
-        'side': 'sell', 'price': 125, 'amount': 2.12},
-]
+# fake_trades = [
+#     {'id': 1, 'user_id': 1, 'currency': 'BTC',
+#         'side': 'buy', 'price': 123, 'amount': 2.12},
+#     {'id': 2, 'user_id': 1, 'currency': 'BTC',
+#         'side': 'sell', 'price': 125, 'amount': 2.12},
+# ]
 
 
-class Trade(BaseModel):
-    id: int
-    user_id: int
-    currency: str = Field(max_length=5)
-    side: str
-    price: float = Field(ge=0)
-    amount: float
+# class Trade(BaseModel):
+#     id: int
+#     user_id: int
+#     currency: str = Field(max_length=5)
+#     side: str
+#     price: float = Field(ge=0)
+#     amount: float
 
 
-@app.post('/trades')
-def add_trades(trades: List[Trade]):
-    fake_trades.extend(trades)
-    return {'status': 200, 'data': fake_trades}
+# @app.post('/trades')
+# def add_trades(trades: List[Trade]):
+#     fake_trades.extend(trades)
+#     return {'status': 200, 'data': fake_trades}
 
 
 # fake_users_2 = [
